837a49c0a2ea69295a51d60e43fcbff94dafa2af
Dineth Hettiarachchi <63235220+DDH13@users.noreply.github.com>
2022-12-16 02:24:25 +0530

    changed customer auth css

--- app/views/signup.view.php
+++ app/views/signup.view.php
@@ -1,45 +1,60 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Signin Template · Bootstrap v5.1</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Signin Template · Bootstrap v5.1</title>
 
 
 
-    <style>
-      input{
-        padding:15px;
-        margin:10px;
-      }
+  <style>
+    body {
+      background: rgb(255, 72, 36);
+      background: linear-gradient(90deg, rgba(255, 72, 36, 1) 7%, rgba(221, 179, 179, 1) 47%, rgba(255, 50, 10, 1) 87%);
+    }
+  </style>
+  <link rel="stylesheet" href="<?= ASSETS ?>/css/dineth.css">
+</head>
 
-    </style>
-
-  </head>
-  <body class="text-center">
+<body class="text-center">
 
   <form method="post">
 
-    <?php if(!empty($errors)):?>
+    <?php if (!empty($errors)) : ?>
       <div class="alert alert-danger">
-        <?= implode("<br>", $errors)?>
+        <?= implode("<br>", $errors) ?>
       </div>
-    <?php endif;?>
-  
+    <?php endif; ?>
+
     <h1>Create account</h1>
 
-    <input name = "first_name" type ="text" placeholder = "First Name">
-    <input name = "last_name" type ="text" placeholder = "Last Name">
-    <input type = "text" name= "contactNo" placeholder="Contact No">
-    <input name = "email" type= "text" placeholder="Email" >
-    <input name = "password" type ="password" placeholder = "Password">
-    <input name = "password_confirm" type ="password" placeholder = "Confirm Password">
+    <div class="form__group field">
+      <input name="first_name" type="text" placeholder="First Name" class="form__field">
+    </div>
+    <div class="form__group field">
+      <input name="last_name" type="text" placeholder="Last Name" class="form__field">
+    </div>
+    <div class="form__group field">
+      <input type="text" name="contactNo" placeholder="Contact No" class="form__field">
+    </div>
+    <div class="form__group field">
+      <input name="email" type="text" placeholder="Email" class="form__field">
+    </div>
+    <div class="form__group field">
+      <input name="password" type="password" placeholder="Password" class="form__field">
+    </div>
+    <div class="form__group field">
+      <input name="password_confirm" type="password" placeholder="Confirm Password" class="form__field">
+    </div>
+
 
     <button type="submit">Create</button>
-    <a href="<?=ROOT?>">Home</a>
-    <a href="<?=ROOT?>/auth/login">Login</a>
+    <a href="<?= ROOT ?>">Home</a>
+    <a href="<?= ROOT ?>/auth/login">Login</a>
 
   </form>
-  </body>
+</body>
+
 </html>
