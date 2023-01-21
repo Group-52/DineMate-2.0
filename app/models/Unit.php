fdd092e5b8de000c0b51ed8500bc9772a999348c
Dineth Hettiarachchi <63235220+DDH13@users.noreply.github.com>
2023-01-21 17:44:01 +0530

    .

--- app/models/Unit.php
+++ app/models/Unit.php
@@ -6,7 +6,24 @@ class Unit extends Model
     {
         $this->table = "units";
         $this->columns = [
-            "name"
+            "unit_id",
+            "unit_name",
+            "abbreviation",
+            "type"
         ];
     }
+
+    // add a unit
+    public function addUnit($unit_name, $type, $abbreviation=null)
+    {
+        $data = [
+            "unit_name" => $unit_name,
+            "abbreviation" => $abbreviation,
+            "type" => $type
+        ];
+        return $this->insert($data);
+    }
+
+    // convert unit
+
 }
