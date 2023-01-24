b01a39d1abbb72bed4a32ee08a21ac3b95832b5c
KeethapriyaM <87429872+KeethapriyaM@users.noreply.github.com>
2023-01-24 22:37:30 +0530

    created employee view

---
+++ app/views/employee.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Employee View</title>
+    
+</head>
+
+<body>
+    <h2>Employees</h2>
+    <button type="submit" name="add"><a href="#">+ NEW EMPLOYEE</a></button><br>
+
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">EmployeeID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Role</th>
+                <th scope="col">Salary</th>
+                <th scope="col">DOB</th>
+                <th scope="col">Contact No</th>
+                <th scope="col">NIC</th>
+            </tr>
+        </thead>
+    </table>
+    <br> 
+</body>
+
+</html>
