3e84fb67eed3c32f7cb284c1a89d2e59c0c9ab77
KeethapriyaM <87429872+KeethapriyaM@users.noreply.github.com>
2023-01-24 22:38:36 +0530

    created menu view

---
+++ app/views/menu.update.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Update Menu</title>
+    <style>
+        
+    </style>
+    
+</head>
+
+<body>
+    <h2>Menus</h2>
+    <button type="submit" name="save"><a class="link" href="#">Save</a></button>
+
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">MenuID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Preparing Time</th>
+                <th scope="col">Selling Price</th>
+                <th scope="col">Net Price</th>
+            </tr>
+        </thead>
+    
+    </table>
+    <br>
+        <button type="submit" name="add"><a href="#">+ ADD DISH</a></button><br>
+        <button type="submit" name="delete"><a href="#">+ DELETE DISH</a></button><br>
+        
+
+</html>
