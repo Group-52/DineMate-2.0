86b53c6eb279963546226377057470dbf1af4597
ZulfaNasry <zulfanasry1999@gmail.com>
2023-01-27 21:40:31 +0530

    add css to editt page

--- app/views/order.edit.view.php
+++ app/views/order.edit.view.php
@@ -1,3 +1,4 @@
+<?php include "partials/dashboard.header.php" ?>
 <!DOCTYPE html>
 
 
@@ -10,21 +11,21 @@
 <body>
 
 <?php if (isset($order)): ?>
-    <h1>Order #<?= $order->order_id ?></h1>
+    <h2>Order #<?= $order->order_id ?></h2>
     <form method="POST">
-        <label>Order ID</label> <br>
+        <label>Order ID</label> 
         <input type="text" name="order_id" value="<?= $order->order_id ?>" readonly><br>
-        <label>Guest ID</label> <br>
+        <br><label>Guest ID</label> 
         <input type="text" name="reg_customer_id" value="<?= $order->reg_customer_id ?? $guest_id ?? "" ?>"
                readonly><br>
-        <label>Request</label> <br>
+       <br> <label>Request</label> 
         <input type="text" name="request" value="<?= $order->request ?>"><br>
-        <label>Quantity</label> <br>
+       <br> <label>Quantity</label>
         <input type="number" name="quantity" value="<?= $order->quantity ?>"><br>
-        <label>Time Placed</label> <br>
+        <br><label>Time Placed</label> 
         <input type="datetime-local" name="time_placed" value="<?= $order->time_placed ?>"><br>
 
-        <label>Type</label> <br>
+        <br><label>Type</label> 
         <?php
         $order_types = ["Dine-In", "Takeaway"];
         ?>
@@ -34,7 +35,7 @@
             <?php endforeach; ?>
         </select> <br>
 
-        <label>Status</label> <br>
+       <br> <label>Status</label>
         <?php
         $statuses = ["Pending", "Accepted", "Cancelled", "Completed"];
         ?>
@@ -44,9 +45,9 @@
             <?php endforeach; ?>
         </select> <br>
 
-        <label>Scheduled Time</label> <br>
+       <br> <label>Scheduled Time</label> 
         <input type="datetime-local" name="scheduled_time" value="<?= $order->scheduled_time ?>"><br>
-        <label>Table ID</label> <br>
+       <br> <label>Table ID</label> 
         <input type="text" name="table_id" value="<?= $order->table_id ?>"><br>
         <button>Update</button>
     </form>
@@ -59,7 +60,60 @@
 </body>
 </div>
 </html>
+<?php include "partials/dashboard.footer.php" ?>
 
 <style>
+body {
+  font-family: Arial, sans-serif;
+}
+
+form {
+  width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 2px solid #588c7e;
+}
+
+label {
+  display: block;
+  margin-bottom: 10px;
+  font-weight: bold;
+  color: #588c7e;
+}
+
+input[type="text"], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 1px solid #588c7e;
+  border-radius: 4px;
+}
+
+input[type="number"] {
+  width: 30%;
+}
+
+input[type="datetime-local"] {
+  width: 100%;
+}
+
+button {
+  width: 50%;
+  background-color:#588c7e;
+  color: white;
+  padding: 20px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  
+ 
+}
+
+button:hover {
+  background-color: #c0392b;
+}
+
+
 
 </style>
