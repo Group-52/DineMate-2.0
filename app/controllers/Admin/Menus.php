f40b4e690298c76ca536870110d2468d3b5fd2c8
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-01-28 01:28:01 +0530

    add menu

--- app/controllers/Admin/Menus.php
+++ app/controllers/Admin/Menus.php
@@ -1,14 +1,18 @@
 <?php
 
+namespace controllers\admin;
+
+use core\Controller;
+use models\Menu;
+
 /**
  * Dish Class
  */
-
 class Menus
 {
     use Controller;
 
-    public function index()
+    public function index(): void
     {
         $m = new Menu();
         $results['menulist'] = $m->getMenus();
