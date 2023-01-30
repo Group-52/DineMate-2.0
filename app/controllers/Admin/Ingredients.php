be395d3b9595c4d703b99069c1fc418ddb5086db
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-01-30 23:43:39 +0530

    fix ingredients view

--- app/controllers/Admin/Ingredients.php
+++ app/controllers/Admin/Ingredients.php
@@ -24,59 +24,8 @@ class Ingredients
         $units = $u->getUnits();
 
         $ing = new Ingredient();
-        $ingredientList = $ing->getAllIngredients();
+        $ingredientList = $ing->getIngredients();
 
         $this->view('admin/ingredients', ['ingredients' => $ingredients, 'dishes' => $dishes, 'units' => $units, 'ingredientList' => $ingredientList]);
     }
-
-    public function add(): void
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $m = new Ingredient();
-
-            $data = json_decode(file_get_contents("php://input"), true);
-            $dishId = $data['dish'];
-            $itemId = $data['ingredient'];
-            $unitId = $data['unit'];
-            $amount = $data['quantity'];
-
-            $m->addIngredient($dishId, $itemId, $amount, $unitId);
-            echo json_encode(array("status" => "success", "message" => "Data received successfully"));
-        } else
-            echo json_encode(array("status" => "error", "message" => "Invalid request"));
-
-    }
-
-    public function delete(): void
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $m = new Ingredient();
-
-            $data = json_decode(file_get_contents("php://input"), true);
-            $dish = $data['dish'];
-            $ingredient = $data['ingredient'];
-
-            $m->deleteIngredient($dish, $ingredient);
-            echo json_encode(array("status" => "success", "message" => "Data received successfully"));
-        } else
-            echo json_encode(array("status" => "error", "message" => "Invalid request"));
-    }
-
-    public function edit(): void
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $m = new Ingredient();
-
-            $data = json_decode(file_get_contents("php://input"), true);
-            $dish = $data['dish'];
-            $ingredient = $data['ingredient'];
-            $amount = $data['quantity'];
-            $unit = $data['unit'];
-
-            $m->updateIngredient($dish, $ingredient, $amount, $unit);
-            echo json_encode(array("status" => "success", "message" => "Data received successfully"));
-        } else
-            echo json_encode(array("status" => "error", "message" => "Invalid request"));
-    }
-
 }
--- app/views/partials/admin/sidebar.partial.php
+++ app/views/partials/admin/sidebar.partial.php
@@ -1,20 +1,24 @@
 <div id="sidebar" class="sidebar p-5 pt-1">
-        <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "home") echo " active" ?>" href="<?= ROOT ?>/admin">
-                <i class="fa-solid fa-house d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Dashboard</span>
-        </a>
-        <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "items") echo " active" ?>" href="<?= ROOT ?>/admin/items">
-                <i class="fa-solid fa-sitemap d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Items</span>
-        </a>
-        <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && ($controller == "inventory" || $controller == "inventory2")) echo " active" ?>" href="<?= ROOT ?>/admin/inventory">
-                <i class="fa-solid fa-box d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Inventory</span>
-        </a>
-        <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "ingredients") echo " active" ?>" href="<?= ROOT ?>/admin/ingredients">
-                <i class="fa-solid bi bi-egg-fried d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Ingredients</span>
-        </a>
-        <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "menus") echo " active" ?>" href="<?= ROOT ?>/admin/menus">
-                <i class="fa-solid custom-icon d-inline" style="background-image:url('<?=ASSETS?>/images/icons/food-menu.svg')"></i>
-                <span class="sidebar-text">&nbsp;&nbsp;Menus</span>
-        </a>
+    <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "home") echo " active" ?>"
+       href="<?= ROOT ?>/admin">
+        <i class="fa-solid fa-house d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Dashboard</span>
+    </a>
+    <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "items") echo " active" ?>"
+       href="<?= ROOT ?>/admin/items">
+        <i class="fa-solid fa-sitemap d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Items</span>
+    </a>
+    <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && ($controller == "inventory" || $controller == "inventory2")) echo " active" ?>"
+       href="<?= ROOT ?>/admin/inventory">
+        <i class="fa-solid fa-box d-inline"></i><span class="sidebar-text">&nbsp;&nbsp;Inventory</span>
+    </a>
+    <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "ingredients") echo " active" ?>"
+       href="<?= ROOT ?>/admin/ingredients">
+        <i class="fa-solid fa-carrot"></i><span class="sidebar-text">&nbsp;Ingredients</span>
+    </a>
+    <a class="fw-bold d-block mb-4 fs-4<?php if (isset($controller) && $controller == "menus") echo " active" ?>"
+       href="<?= ROOT ?>/admin/menus">
+        <i class="fa-solid fa-utensils"></i><span class="sidebar-text">&nbsp;&nbsp;Menus</span>
+    </a>
 
 
 </div>
