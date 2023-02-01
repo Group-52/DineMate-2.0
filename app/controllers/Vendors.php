08c002098573a172aded2e2d0ccb33e01697b9a6
KeethapriyaM <keethapriya.m@gmail.com>
2023-02-01 22:30:56 +0530

    update vendor model and controller

--- app/controllers/Vendors.php
+++ app/controllers/Vendors.php
@@ -1,37 +1,67 @@
 <?php
 
-// vendor class
+namespace controllers\admin;
 
+use core\Controller;
+use Exception;
+use models\Vendor;
+// use models\Item;
+// use models\Unit;
+
+/**
+ * Items Controller
+ */
 class Vendors
 {
     use Controller;
 
-    public function index()
+    private string $controller = "vendors";
+
+    public function index(): void
     {
-        $vendor = new Vendor;
-        $results['Vendor'] = $vendor->getVendors();      
-        $this->view('vendor', $results);
+        if (!isset($_SESSION["user"])) {
+            redirect("admin/auth");
+        }
+        $data = [];
+        // $data["items"] = (new Item())->itemsSearch($_GET);
+        // $data["categories"] = (new Category())->select()->fetchAll();
+        // $data["query"] = $_GET["query"] ?? "";
+        // $data["category_name"] = $_GET["category"] ?? "";
+
+        $data["controller"] = $this->controller;
+        $this->view("admin/vendor", $data);
     }
 
-    public function addVendor(): void
+    public function create(): void
     {
-        if(isset($_POST['save'])){
-			$name = $_POST['name'];
-			$address = $_POST['address'];
-			$company = $_POST['company'];
-			$contact_no = $_POST['contact_no'];
+        /** TODO
+         * Add form component
+         */
 
-			$vendor = new Vendor;
-			$vendor ->addVendor([
-				'name'=> $name,
-				'address'=> $address,
-				'company'=> $company,
-                'contact_no'=> $contact_no
-			]);
-
-            redirect('vendors');
-
+        if (!isset($_SESSION["user"])) {
+            redirect("admin/auth");
         }
-        $this->view('vendor.add');
+
+        $data = [];
+        // $data["categories"] = (new Category())->select()->fetchAll();
+        // $data["units"] = (new Unit())->select()->fetchAll();
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $item = new vendor();
+            if ($item->validate($_POST)) {
+                try {
+                    $item->insert([
+                        "name" => $_POST["name"],
+                        "address" => $_POST["address"] ?? null,
+                        "company" => $_POST["company"] ?? null,
+                        "contact_no" => $_POST["contact_no"]
+                    ]);
+                    redirect("admin/vendors");
+                } catch (Exception $e) {
+                    $data["error"] = "Unknown error.";
+                }
+            }
+        }
+        $data["controller"] = $this->controller;
+        $this->view("admin/vendor.add", $data);
     }
 }
