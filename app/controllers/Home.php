3c2ff505e115387606cf2a3bc73bc44c03cd6a7f
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-02-01 08:48:40 +0530

    temporary fix to employee breaking page when logged into home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -14,18 +14,21 @@ class Home
 
     public function index(): void
     {
-        $data['user'] = $_SESSION['user'] ?? null;
+        if (!isset($_SESSION['user']->emp_id)) {
+            $data['user'] = $_SESSION['user'] ?? null;
 
-        /** TODO
-         * Get number of items in cart
-         */
+            /** TODO
+             * Get number of items in cart
+             */
+            # Get Menu From Database
+            $menus = (new Menu())->getMenus();
+            foreach ($menus as $menu) {
+                $data['menus'][$menu->menu_id] = new \components\Menu($menu, 4);
+            }
 
-        # Get Menu From Database
-        $menus = (new Menu())->getMenus();
-        foreach ($menus as $menu) {
-            $data['menus'][$menu->menu_id] = new \components\Menu($menu, 4);
+            $this->view('home', $data);
+        } else {
+            redirect("admin");
         }
-
-        $this->view('home', $data);
     }
 }
