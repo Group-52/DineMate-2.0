971bb8b28a8f53145503910ffffef856f36bf1bb
Dineth Hettiarachchi <63235220+DDH13@users.noreply.github.com>
2023-02-05 21:51:55 +0530

    changed UI look, shifted css from styles.css to inventory.css, separated APIs

--- app/views/admin/inventory.view.php
+++ app/views/admin/inventory.view.php
@@ -5,6 +5,7 @@
   <?php include VIEWS . "/partials/admin/head.partial.php" ?>
   <link rel="stylesheet" href="<?= ASSETS ?>/css/admin/common.css">
   <link rel="stylesheet" href="<?= ASSETS ?>/css/admin/inventory.css">
+  <script src="<?= ASSETS ?>/js/admin/inventory.js"></script>
 </head>
 
 <body class="dashboard">
@@ -12,9 +13,11 @@
   <div class="dashboard-container">
     <?php include VIEWS . "/partials/admin/sidebar.partial.php" ?>
     <div class="w-100 h-100 p-5">
-      <div class="dashboard-header">
-
-        <h1 class="display-3 active">Inventory</h1>
+      <div class="dashboard-header d-flex flex-row align-items-center justify-content-space-between w-100">
+        <h1 class="display-3">Inventory</h1>
+        <a class="btn btn-primary text-uppercase fw-bold" href="#" id="finish-button">Finish Editing</a>
+        <a class="btn btn-primary text-uppercase fw-bold" href="#" id="edit-button">Edit</a>
+        <a class="btn btn-primary text-uppercase fw-bold" href="<?= ROOT ?>/admin/inventory/info" id="switch-button">Detailed View</a>
       </div>
 
       <table class="table">
@@ -34,95 +37,24 @@
           <?php if (isset($inventory)) : ?>
             <?php foreach ($inventory as $item) : ?>
               <tr data-item-id="<?= $item->item_id ?>">
-                <td data-field-name="item_name"><?= $item->item_name ?></td>
-                <td data-field-name="amount_remaining"><?= $item->amount_remaining ?></td>
-                <td data-field-name="last_updated"><?= $item->last_updated ?></td>
+                <td><?= $item->item_name ?></td>
+                <td><?= $item->amount_remaining ?></td>
+                <td><?= $item->last_updated ?></td>
                 <td data-field-name="max_stock_level"><?= $item->max_stock_level ?></td>
                 <td data-field-name="buffer_stock_level"><?= $item->buffer_stock_level ?></td>
                 <td data-field-name="reorder_level"><?= $item->reorder_level ?></td>
                 <td data-field-name="lead_time"><?= $item->lead_time ?></td>
+                <td><i class="fa fa-pencil-square-o edit-icon" aria-hidden="true"></i></td>
+                <td><i class="fa fa-check-circle tick-icon edit-options" aria-hidden="true"></i></td>
+                <td><i class="fa fa-times-circle cross-icon edit-options" aria-hidden="true"></i></td>
               </tr>
             <?php endforeach; ?>
           <?php endif; ?>
         </tbody>
       </table>
-      <a href="#" onclick="makeEditable()" id="editButton" class='btn btn-primary'>Edit</a>
-      <a href="#" onclick="updateInventory()" class='btn btn-primary' id="updateButton">Update</a>
-      <a href="<?= ROOT ?>/admin/inventory/info" class='btn btn-primary' id="switch-button">Detailed View</a>
     </div>
   </div>
 </body>
 
 </html>
-
-<script>
-  function makeEditable() {
-    var cells = document.querySelectorAll("td[data-field-name='max_stock_level'], td[data-field-name='buffer_stock_level'], td[data-field-name='reorder_level'], td[data-field-name='lead_time']");
-    for (var i = 0; i < cells.length; i++) {
-      cells[i].setAttribute("contenteditable", "true");
-      cells[i].classList.add("editable");
-      cells[i].addEventListener("input", function() {
-        this.setAttribute("data-changed", true);
-      })
-    }
-    // make upadate button visible
-    document.getElementById("updateButton").style.display = "inline-block";
-
-    // focus on first editable cell
-    cells[0].focus();
-  }
-
-  function makeUneditable() {
-    var cells = document.querySelectorAll("td[data-field-name='max_stock_level'], td[data-field-name='buffer_stock_level'], td[data-field-name='reorder_level'], td[data-field-name='lead_time']");
-    for (var i = 0; i < cells.length; i++) {
-      cells[i].setAttribute("contenteditable", "false");
-      cells[i].classList.remove("editable");
-    }
-
-    // hide update button
-    document.getElementById("updateButton").style.display = "none";
-    // make editable button visible
-    document.getElementById("editButton").style.display = "inline-block";
-  }
-
-  function updateInventory() {
-    // Collect data from editable cells
-    var cells = document.querySelectorAll(".editable");
-    var data = [];
-    for (var i = 0; i < cells.length; i++) {
-      var row = cells[i].parentNode;
-      var itemid = row.getAttribute("data-item-id");
-      var fieldName = cells[i].getAttribute("data-field-name");
-      var newValue = cells[i].innerHTML;
-      if (cells[i].hasAttribute('data-changed')) {
-        data.push({
-          itemid: itemid,
-          fieldName: fieldName,
-          newValue: newValue
-        });
-      }
-    }
-
-    makeUneditable();
-
-
-    // Send data to server
-
-    let fetchRes = fetch(
-      "<?= ROOT ?>/admin/inventory/updateMain", {
-        method: "POST",
-        credentials: 'same-origin',
-        mode: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(data)
-      });
-
-    fetchRes.then(res => res.json())
-      .catch(err => {
-        console.log(err)
-      })
-
-  }
-</script>
+<!-- <script src="<?= ASSETS ?>/js/admin/inventory.js"></script> -->
---
+++ public/assets/js/admin/inventory.js
@@ -0,0 +1,216 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var editbutton = document.querySelector("#edit-button");
+    editbutton.addEventListener("click", makeEditable);
+    var finishbutton = document.querySelector("#finish-button");
+    finishbutton.addEventListener("click", makeUneditable);
+    const pencilIcons = document.querySelectorAll(".edit-icon");
+
+    const fieldNames = ['max_stock_level', 'buffer_stock_level', 'reorder_level', 'lead_time'];
+
+
+
+    function makeEditable() {
+        // Hide the edit button
+        editbutton.style.display = "none";
+        // Show the update button
+        finishbutton.style.display = "inline-block";
+
+        // Show the trash can icon
+        const trashIcons = document.querySelectorAll(".trash-icon");
+        for (i = 0; i < trashIcons.length; i++) {
+            trashIcons[i].parentNode.style.display = "inline-block";
+        }
+        // Show the pencil icon
+        const pencilIcons = document.querySelectorAll(".edit-icon");
+        for (i = 0; i < pencilIcons.length; i++) {
+            pencilIcons[i].parentNode.style.display = "inline-block";
+        }
+    }
+
+    function makeUneditable() {
+        // Show the edit button
+        editbutton.style.display = "inline-block";
+        // Hide the update button
+        finishbutton.style.display = "none";
+
+        // Hide the trash can icon
+        const trashIcons = document.querySelectorAll(".trash-icon");
+        for (i = 0; i < trashIcons.length; i++) {
+            trashIcons[i].parentNode.style.display = "none";
+        }
+        // Hide the pencil icon
+        const pencilIcons = document.querySelectorAll(".edit-icon");
+        for (i = 0; i < pencilIcons.length; i++) {
+            pencilIcons[i].parentNode.style.display = "none";
+        }
+    }
+
+    // TODO fix this function
+    function updateInventory() {
+        let newValue;
+        let pid;
+        let row;
+        let i;
+        // Collect data from editable cells
+        const cells = document.querySelectorAll(".editable");
+        const data = [];
+
+        //Collect checkboxes that were changed
+        const checkboxes = document.querySelectorAll("input[name='expiryrisk']");
+        for (i = 0; i < checkboxes.length; i++) {
+            row = checkboxes[i].parentNode.parentNode;
+            pid = row.getAttribute("data-purchase-id");
+            newValue = checkboxes[i].value;
+            console.log(`pid: ${pid}, newValue: ${newValue}, data-changed: ${checkboxes[i].hasAttribute('data-changed')}`)
+            if (checkboxes[i].hasAttribute('data-changed')) {
+                data.push({
+                    pid: pid,
+                    fieldName: 'expiryrisk',
+                    newValue: newValue
+                });
+            }
+        }
+
+        // Collect data from other grid cells that were changed
+        for (i = 0; i < cells.length; i++) {
+            row = cells[i].parentNode;
+            pid = row.getAttribute("data-purchase-id");
+            const fieldName = cells[i].getAttribute("data-field-name");
+            newValue = cells[i].innerHTML;
+            if (cells[i].hasAttribute('data-changed')) {
+                data.push({
+                    pid: pid,
+                    fieldName: fieldName,
+                    newValue: newValue
+                });
+            }
+        }
+
+        makeUneditable();
+
+        // Send data to server
+        let fetchRes = fetch(
+            "<?= ROOT ?>/admin/inventory/updateInventory", {
+            method: "POST",
+            credentials: 'same-origin',
+            mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        });
+
+        fetchRes.then(res => res.json())
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    // attach click event listener to each pencil icon to make pencil icon disappear and make the cell editable and tick,cross icon appear
+    // make only amount remaining, special notes and expiry risk editable
+
+    pencilIcons.forEach(icon => {
+        icon.addEventListener('click', function (event) {
+            // make tick and cross icon visible
+            event.target.parentNode.parentNode.querySelector('.tick-icon').parentNode.style.display = 'inline-block';
+            event.target.parentNode.parentNode.querySelector('.cross-icon').parentNode.style.display = 'inline-block';
+            // make all pencil icon invisible
+            event.target.parentNode.style.display = 'none';
+
+            const row = event.target.parentNode.parentNode;
+            row.classList.add('row-in-form');
+
+            const cells = row.querySelectorAll('td');
+            cells.forEach(cell => {
+                if (fieldNames.includes(cell.dataset.fieldName)) {
+                    cell.classList.add('editable');
+
+                    const currentValue = cell.textContent;
+                    cell.innerHTML = '';
+
+                    const input = document.createElement('input');
+                    input.type = 'number';
+                    input.value = currentValue;
+                    cell.appendChild(input);
+
+                }
+            });
+        });
+    });
+
+    // Attach click event listener to each tick and cross icon to make the cell uneditable and pencil icon appear
+    const editIcons = document.querySelectorAll('.edit-options');
+    editIcons.forEach(icon => {
+        icon.addEventListener('click', function (event) {
+            // make tick and cross icon invisible
+            event.target.parentNode.parentNode.querySelector('.tick-icon').parentNode.style.display = 'none';
+            event.target.parentNode.parentNode.querySelector('.cross-icon').parentNode.style.display = 'none';
+            // make pencil icon visible
+            event.target.parentNode.parentNode.querySelector('.edit-icon').parentNode.style.display = 'inline-block';
+
+            const row = event.target.parentNode.parentNode;
+            row.classList.remove('row-in-form');
+
+            const cells = row.querySelectorAll('td');
+            cells.forEach(cell => {
+                if (fieldNames.includes(cell.dataset.fieldName)) {
+                    cell.classList.remove('editable');
+
+                    const input = cell.querySelector('input');
+                    cell.textContent = input.value;
+                }
+            });
+        });
+    }
+    );
+
+    // Attach click event listener to each tick icon to update the database
+    const tickIcons = document.querySelectorAll('.tick-icon');
+    tickIcons.forEach(icon => {
+        icon.addEventListener('click', function (event) {
+            updateInventory(icon);
+        });
+    });
+
+
+    function updateInventory(icon) {
+
+        var data = [];
+
+        // collect data from the row
+        let row = icon.parentNode.parentNode;
+        let cells = row.querySelectorAll('td');
+        let id = row.getAttribute("data-item-id");
+        for (i = 0; i < cells.length; i++) {
+            const fieldName = cells[i].getAttribute("data-field-name");
+            if (fieldName) {
+                newValue = cells[i].innerHTML;
+                data.push({
+                    itemid: id,
+                    fieldName: fieldName,
+                    newValue: newValue
+                });
+            }
+        }
+
+        // Send data to server
+        let fetchRes = fetch(
+            `${ROOT}/api/inventory/updateMain`, {
+            method: "POST",
+            credentials: 'same-origin',
+            mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        });
+
+        fetchRes.then(res => res.json())
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+});
