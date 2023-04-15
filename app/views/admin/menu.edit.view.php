5b1d32d014d1883a90814b3e96cf6ef66335a82c
KeethapriyaM <keethapriya.m@gmail.com>
2023-04-15 17:06:51 +0530

    changed some errors

--- app/views/admin/menu.edit.view.php
+++ app/views/admin/menu.edit.view.php
@@ -2,6 +2,10 @@
 <html lang="en">
 <head>
 <?php include VIEWS . "/partials/admin/head.partial.php" ?>
+<link rel="stylesheet" href="<?= ROOT ?>/assets/css/admin/menus.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>/css/admin/tables.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>/css/admin/common.css">
+    <script src="<?= ROOT ?>/assets/js/admin/menus.js"></script>
 </head>
 <body class="dashboard">
 <?php include VIEWS . "/partials/admin/navbar.partial.php" ?>
@@ -14,7 +18,6 @@
             <div class="dashboard-header d-flex flex-row align-menus-center justify-content-space-between w-100">
                 <h1 class="display-4"><a class="link" href="<?= ROOT ?>/admin/menus">Menus</a> > Edit Menu</h1>
                 <button class="btn btn-success text-uppercase fw-bold" type="submit" href="<?= ROOT ?>/admin/menus">Update Menu</button>
-                <button type="button" class="btn btn-secondary" id="cancel-button">Cancel</button>
             </div>
             <div class="form-group">
                 <label class="label" for="menu_id">Menu ID</label>
@@ -30,7 +33,7 @@
             </div>
             <div class="form-group">
                 <label class="label" for="start_time">From Time</label>
-                <input class="form-control" type="time" name="start_time" value="<?= $m->start_time ?>">
+                <input class="form-control" type="time" name="start_time" id="end_time" value="<?= $m->start_time ?>">
             </div>
             <div class="form-group">
                 <label class="label" for="end_time">To Time</label>
