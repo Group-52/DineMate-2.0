70674faa50b9668a1e052cf78ee9c2a534619682
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-04-18 16:57:37 +0530

    add checkout options and details to file checkout.view.php add checkout.js for dynamic updates

--- app/controllers/Checkout.php
+++ app/controllers/Checkout.php
@@ -3,6 +3,9 @@
 namespace controllers;
 
 use core\Controller;
+use components\Form;
+use models\Cart;
+use models\Order;
 
 class Checkout
 {
@@ -10,14 +13,44 @@ class Checkout
 
     public function index(): void
     {
-        if (isset($_SESSION["user"])) {
-            $cart = new \models\Cart();
-            if ($cart->getNoOfItems() == 0) redirect("cart");
-            $data["cart"] = $cart->getCartItems();
-            $data["title"] = "Checkout";
-            $this->view("checkout", $data);
-        } else {
-            redirect("login");
+        $data = [];
+        $data["title"] = "Checkout";
+        $cart = new Cart();
+        $form = new Form("", "POST", "Checkout");
+        $form->addSelectField("order-type", "order-type", "Type of Order",true, ["dine-in" => "Dine-In", "takeaway" => "Take Away"], "Type of Order");
+        $form->addInputField("schedule-order", "schedule-order", "checkbox", "Schedule Order", false, "Schedule Order", "", true);
+        $form->addInputField("order-time", "order-time", "datetime-local", "Order Time", false, "Order Time", date("Y-m-d H:i"), true);
+        $form->addInputField("table-number", "table-number", "number", "Table Number", false, "", "", true);
+        $form->addInputField("request", "request", "text", "Request", false, "Less spicy, no salt, etc.");
+        if (!isset($_SESSION["user"])) {
+            $form->addInputField("full-name", "full-name", "text", "Full Name", true, "Full Name");
+            $form->addInputField("mobile", "mobile", "text", "Mobile", true, "Mobile");
+            $form->addInputField("email", "email", "email", "Email", true, "Email");
         }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($form->isValid($_POST)) {
+                $cart = new Cart();
+                $cart_items = $cart->getCartItems();
+                $order = new Order();
+                $order->create($_POST["order-type"], $cart_items, $_SESSION["user"]->user_id ?? null,
+                    $_POST["request"], $_POST["user_id"] ?? null, $_POST["table-number"] ?? null,
+                    isset($_POST["schedule-order"]) ? $_POST["order-time"] : null);
+
+                if ($order->getErrors()) {
+                    $data["errors"] = $order->getErrors();
+
+                } else {
+                    $data["success"] = "Order placed successfully";
+                    if ($_SESSION["user"])
+                        $cart->clearCart($_SESSION["user"]->user_id);
+                }
+            } else {
+                $data["errors"] = $form->getErrors();
+            }
+        }
+        $data["form"] = $form;
+        $data["cart"] = $cart->getCartItems();
+        $this->view("checkout", $data);
     }
 }
--- app/views/checkout.view.php
+++ app/views/checkout.view.php
@@ -4,13 +4,72 @@
 <head>
     <?php include VIEWS . "/partials/home/head.partial.php" ?>
 </head>
+
 <body class="body-min-vh-100">
 <div class="wrapper">
-    <?php include VIEWS . "/partials/home/navbar.partial.php" ?>
+    <?php include VIEWS . "/partials/home/navbar.partial.php"; ?>
     <div class="container my-5">
-        <h1 class="display-4">Checkout</h1>
+        <h1 class="display-4 mb-3">Order Details</h1>
+        <?php if (isset($form)) : ?>
+            <?php
+            /** @var components\Form $form */
+            $form->beginForm();
+            ?>
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <?php for ($i = 0; $i < 5; $i++) {
+                        echo $form->htmlField($i);
+                    } ?>
+                    <?php if (!isset($_SESSION["user"])): ?>
+                        <h2 class='display-5 mb-3'>Personal Info</h2>
+                        <?php for ($i = 5; $i < $form->countFields(); $i++) {
+                            echo $form->htmlField($i);
+                        } ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col-offset-md-1 col-md-5 col-12">
+                    <h2 class="display-5">Order Summary</h2>
+                    <table id="cart-table" class="table cart">
+                        <thead>
+                        <tr>
+                            <th scope="col">Item</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Price</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php if (isset($cart)): ?>
+                            <?php
+                            $total = 0.00;
+                            foreach ($cart as $item) : ?>
+                                <tr>
+                                    <td><?= $item->dish_name ?></td>
+                                    <td><?= $item->quantity ?></td>
+                                    <?php
+                                    $price = (float)$item->selling_price * (float)$item->quantity;
+                                    $total += $price;
+                                    ?>
+                                    <td><?= $price ?></td>
+                                </tr>
+                            <?php endforeach ?>
+                            <tr class="secondary fs-4">
+                                <td colspan="2" class="fw-bold">TOTAL</td>
+                                <td class="fw-bold"><?= $total ?></td>
+                            </tr>
+                        <?php endif; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="mt-5">
+                <?= $form->htmlButton(false) ?>
+            </div>
+            <?php
+            $form->endForm();
+        endif; ?>
     </div>
 </div>
-<?php include VIEWS . "/partials/home/footer.partial.php" ?>
+<script src="<?= ASSETS ?>/js/checkout.js"></script>
+<?php include VIEWS . "/partials/home/footer.partial.php"; ?>
 </body>
 </html>
