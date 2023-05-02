ddd239fc1e0e3d9781daf1d9bac700a45b3405af
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-05-02 17:18:05 +0530

    Add logic to display form depending on type of user in checkout

--- app/controllers/Checkout.php
+++ app/controllers/Checkout.php
@@ -15,7 +15,7 @@ class Checkout
     public function index(): void
     {
         $cart = new Cart();
-        $cartItems = $cart->getCartItems();
+        $cartItems = $cart->getCartItems(userId(), isGuest());
         if (empty($cartItems)) {
             redirect("cart");
         }
@@ -24,11 +24,13 @@ class Checkout
         $data["title"] = "Checkout";
         $form = new Form("", "POST", "Checkout");
         $form->addSelectField("order-type", "order-type", "Type of Order",true, ["dine-in" => "Dine-In", "takeaway" => "Take Away"], "Type of Order");
-        $form->addInputField("schedule-order", "schedule-order", "checkbox", "Schedule Order", false, "Schedule Order", "", true);
-        $form->addInputField("order-time", "order-time", "datetime-local", "Order Time", false, "Order Time", date("Y-m-d H:i"), true);
+        if (isRegistered()) {
+            $form->addInputField("schedule-order", "schedule-order", "checkbox", "Schedule Order", false, "Schedule Order", "", true);
+            $form->addInputField("order-time", "order-time", "datetime-local", "Order Time", false, "Order Time", date("Y-m-d H:i"), true);
+        }
         $form->addInputField("table-number", "table-number", "number", "Table Number", false, "", "", true);
         $form->addInputField("request", "request", "text", "Request", false, "Less spicy, no salt, etc.");
-        if (!isset($_SESSION["user"])) {
+        if (isGuest()) {
             $form->addInputField("full-name", "full-name", "text", "Full Name", true, "Full Name");
             $form->addInputField("mobile", "mobile", "text", "Mobile", true, "Mobile");
             $form->addInputField("email", "email", "email", "Email", true, "Email");
@@ -37,10 +39,10 @@ class Checkout
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($form->isValid($_POST)) {
                 $cart = new Cart();
-                $cart_items = $cart->getCartItems();
+                $cart_items = $cart->getCartItems(userId(), isGuest());
                 $order = new Order();
-                $order_id = $order->create($_POST["order-type"], $cart_items, reg_customer_id: $_SESSION["user"]->user_id ?? null, guest_id: $_POST["user_id"] ?? null,
-                    request: $_POST["request"], table_id: $_POST["table-number"] ?? null,
+                $order_id = $order->create($_POST["order-type"], $cart_items, reg_customer_id: (isRegistered()) ? userId() : null,
+                    guest_id: (isGuest()) ? userId() : null, request: $_POST["request"], table_id: $_POST["table-number"] ?? null,
                     scheduled_time: isset($_POST["schedule-order"]) ? $_POST["order-time"] : null);
 
                 if ($order->getErrors()) {
@@ -56,13 +58,15 @@ class Checkout
                         "status" => "pending",
                         "time_placed" => date("Y-m-d H:i:s"),
                         "request" => $_POST["request"],
-                        "reg_customer_id" => $_SESSION["user"]->user_id ?? null,
+                        "user_id" => userId(),
+                        "user_type" => (isRegistered()) ? "registered" : "guest",
                         "type" => $_POST["order-type"],
+                        "scheduled_time" => $_POST["schedule-order"] ?? null,
                         "table_id" => $_POST["table-number"] ?? null,
                         "order_dishes" => $cart_items
                     ]));
                     if ($_SESSION["user"])
-                        $cart->clearCart($_SESSION["user"]->user_id);
+                        $cart->clearCart(userId(), isGuest());
                 }
             } else {
                 $data["errors"] = $form->getErrors();
--- public/assets/js/checkout.js
+++ public/assets/js/checkout.js
@@ -4,57 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const orderTimeInput = document.getElementById("order-time");
   const tableNumberInput = document.getElementById("table-number");
 
-  scheduleCheckbox.onchange = () =>
-    (orderTimeInput.disabled = !scheduleCheckbox.checked);
+  if (scheduleCheckbox !== null)
+  {
+    scheduleCheckbox.onchange = () =>
+      (orderTimeInput.disabled = !scheduleCheckbox.checked);
+  }
 
   orderTypeSelect.onchange = () => {
     if (orderTypeSelect.value !== "dine-in") {
-      scheduleCheckbox.disabled = false;
+      if (scheduleCheckbox !== null)
+      {
+        scheduleCheckbox.disabled = false;
+        orderTimeInput.disabled = !scheduleCheckbox.checked;
+      }
       tableNumberInput.disabled = true;
-      orderTimeInput.disabled = !scheduleCheckbox.checked;
     } else {
-      scheduleCheckbox.disabled = true;
-      scheduleCheckbox.checked = false;
+      if (scheduleCheckbox !== null)
+      {
+        scheduleCheckbox.disabled = true;
+        scheduleCheckbox.checked = false;
+        orderTimeInput.disabled = true;
+      }
       tableNumberInput.disabled = false;
-      orderTimeInput.disabled = true;
     }
   };
-
-  const checkoutBtn = document.querySelector("button");
-  let socket = new WebSocket("ws://localhost:8080");
-  checkoutBtn.onclick = (e) => {
-    e.preventDefault();
-    socket.onopen = function () {
-      console.log("Event sent");
-      let n = {
-        "event_type": "new_order",
-        "order_id": 25,
-        "status": "pending",
-        "time_placed": Date().toString(),
-        "request": "more food",
-        "reg_customer_id": 112,
-        "type": "dine-in",
-        "table_id": 4,
-        "order_dishes":
-          [
-            {
-              "dish_name": "Burger",
-              "quantity": 2,
-            },
-            {
-              "dish_name": "Chillie Parata",
-              "quantity": 3
-            },
-            {
-              "dish_name": "Salad",
-              "quantity": 1
-            }
-          ]
-
-
-      };
-      socket.send(JSON.stringify(n));
-    };
-  }
-
 });
