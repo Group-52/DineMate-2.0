7f2a918b8bec13dbec3436b525f5295128c69cf7
Nipun Chamikara <nipunchamikara@yahoo.com>
2023-05-04 14:59:11 +0530

    Fix items UI

--- public/assets/js/admin/items.js
+++ public/assets/js/admin/items.js
@@ -9,31 +9,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const category = document.querySelector('select[name="category"]');
     category.onchange = function () {
-        if (category.value == 'All') {
+        if (category.value === 'All') {
             window.location.href = `${ROOT}/admin/items`;
         }
         this.form.submit();
     }
 
+    const formClose = () => {
+        form.querySelector('form').reset();
+        form.style.display = 'none';
+        table.style.filter = 'blur(0)';
+        addButton.style.display = 'block';
+    }
+
     // make form visible when add button is clicked
-    addButton.addEventListener('click', () => {
-        event.preventDefault();
+    addButton.addEventListener('click', (e) => {
+        e.preventDefault();
         form.style.display = 'block';
         table.style.filter = 'blur(5px)';
         // focus on first input
         form.querySelector('input').focus();
         // make add button invisible
         addButton.style.display = 'none';
-
-
     });
 
     // make form invisible when cancel button is clicked
-    cancelButton.addEventListener('click', () => {
-        form.querySelector('form').reset();
-        form.style.display = 'none';
-        table.style.filter = 'blur(0)';
-        addButton.style.display = 'block';
-    });
+    cancelButton.addEventListener('click', formClose);
 
+    document.querySelectorAll(".item-delete").forEach((deleteButton) => {
+        deleteButton.onclick = () => {
+            const itemId = deleteButton.dataset.id;
+            fetch(`${ROOT}/api/items/delete/${itemId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+              .then(response => response.json())
+              .then(result => {
+                 if (result?.status === "success") {
+                     deleteButton.parentElement.parentElement.remove();
+                     new Toast("fa-solid fa-check", "#28a745", "Deleted", "Item has been deleted", false, 3000);
+                 } else {
+                      new Toast("fa-solid fa-times", "#dc3545", "Error", "Item could not be deleted", false, 3000);
+                 }
+              })
+              .catch(error => {
+                  new Toast("fa-solid fa-times", "#dc3545", "Error", "Item could not be deleted", false, 3000);
+              })
+        }
+    })
 });
